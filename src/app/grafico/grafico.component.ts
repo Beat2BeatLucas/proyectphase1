394c70f72a48564c85128e9d0db21fe635d5a086
Lucas Acuna <lucasnahuelnet@gmail.com>
2019-08-09 19:35:47 -0300

    firts commit

---
+++ src/app/grafico/grafico.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import {Grilla} from './grilla';
+
+@Component({
+  selector: 'app-grafico',
+  templateUrl: './grafico.component.html',
+  styleUrls: ['./grafico.component.css']
+})
+
+export class GraficoComponent implements OnInit {
+
+  private width: number;
+  private height: number;
+  private svg: any;
+  private svg2: any;
+
+  data: any[]=[[0, 80],[30, 100],[50, 30],[100, 50],[200, 40],[300, 80],[350,120],[400,50]];
+  data2: any[]=[[0, 180],[30, 200],[50, 130],[100, 150],[200, 140],[300, 180],[350,220],[400,150]];
+
+  constructor() { 
+    this.width= 1002;
+    this.height= 502;
+  }
+
+  ngOnInit() {
+    this.initSvg();
+  }
+
+  private initSvg(){
+    d3.select('#container')
+      .style('margin-left',250 + 'px')
+      .style('width', this.width + 'px')
+      .style('height', this.height + 'px');
+
+    this.svg= d3.select('#container')
+        .append('svg');
+
+    this.svg2= d3.select('#container')
+        .append('svg');
+    
+    let lineGenerator= d3.line();
+    let pathData= lineGenerator(this.data);
+    let pathData2= lineGenerator(this.data2);
+
+    var grilla1= new Grilla(50,100,this.svg,'beige',pathData);
+    this.svg= grilla1.createGrid();
+
+    var grilla2= new Grilla(50,100,this.svg2,'white',pathData2);
+    this.svg2= grilla2.createGrid();
+
+  }
+
+}
---
+++ src/app/grafico/grilla.ts
@@ -0,0 +1,91 @@
+export class Grilla {
+    private grid_height: number;
+    private grid_width: number;
+    private grid_svg: any;
+    private background_color: string;
+    private pathData: string;
+
+    constructor(width: number, height:number, svg: any, color: string, pathData: string){
+        this.grid_width= width;
+        this.grid_height= height;
+        this.grid_svg= svg;
+        this.background_color= color;
+        this.pathData=pathData;
+    }
+
+    public createGrid(){
+        this.initSVG();
+        this.drawGrid();
+        this.drawPath();
+    }
+
+    private initSVG(){
+        this.grid_svg.attr('width', this.grid_width + '%')
+                     .attr('height', this.grid_height + '%')
+                     .style('background-color',this.background_color)
+                     .attr('g');
+    }
+
+    private drawGrid(){
+        var defs= this.grid_svg.append('defs');
+    
+        var pattern1= defs.append('pattern')
+                    .attr('id','smallGrid')
+                    .attr('width',5)
+                    .attr('height',5)
+                    .attr('patternUnits','userSpaceOnUse');
+            pattern1.append('path')
+                    .attr('fill','none')
+                    .attr('stroke','gray')
+                    .attr('stroke-width',0.4)
+                    .attr('d','M 5 0 L 0 0 0 5');
+        
+        
+        var pattern2= defs.append('pattern')
+                    .attr('id','mediumGrid')
+                    .attr('width',25)
+                    .attr('height',25)
+                    .attr('patternUnits','userSpaceOnUse');
+            pattern2.append('rect')
+                    .attr('width',25)
+                    .attr('height',25)
+                    .attr('fill','url(#smallGrid)');
+            pattern2.append('path')
+                    .attr('fill','none')
+                    .attr('stroke','gray')
+                    .attr('stroke-width',1)
+                    .attr('d','M 25 0 L 0 0 0 25');
+    
+        var pattern3= defs.append('pattern')
+                    .attr('id','grid')
+                    .attr('width',125)
+                    .attr('height',125)
+                    .attr('patternUnits','userSpaceOnUse');
+            pattern3.append('rect')
+                    .attr('width',125)
+                    .attr('height',125)
+                    .attr('fill','url(#mediumGrid)');
+            pattern3.append('path')
+                    .attr('fill','none')
+                    .attr('stroke','gray')
+                    .attr('stroke-width',2.5)
+                    .attr('d','M 125 0 L 0 0 0 125');
+
+        this.grid_svg.append('rect')
+                    .attr('x', 0)
+                    .attr('y', 0)
+                    .attr('width','100%')
+                    .attr('height','100%')
+                    .attr('fill','url(#grid)');
+    }
+
+    private drawPath(){
+        this.grid_svg.append('path')
+            .attr('stroke','steelblue')
+            .attr('fill','none')
+            .attr('stroke-width',1)
+            .attr('stroke-linejoin','round')
+            .attr('d', this.pathData);
+      }
+    
+}
